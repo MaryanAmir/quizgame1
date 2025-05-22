b4d318813d91aa759fc16d3bba4c5c2621ae5081
MaryanAmir <150050612+MaryanAmir@users.noreply.github.com>
2025-05-22 16:29:32 +0300

    proj1.py

--- proj1.py
+++ proj1.py
@@ -3,19 +3,19 @@ play =input('Do you want play ?')
 if play == 'yes' :
      print("Great. Let's start")
      score=0
-     answer=input('What is 2 + 2?')#quwstion one********************
+     answer=input('What is 2 + 2?')#question one********************
      if answer == '4': 
         print('correct! youe score is 1')
         score+=1
      else:
         print('incorrect!')
-     answer=input('What is 3+ 22?')#quwstion two********************
+     answer=input('What is 3+ 22?')#question two********************
      if answer == '25': 
         print('correct! youe score is 2')
         score+=1
      else:
         print('incorrect!')
-     answer=input('What is 2 * 2?')#quwstion 3********************
+     answer=input('What is 2 * 2?')#question 3********************
      if answer == '4': 
         print('correct! youe score is 3')
         score+=1
